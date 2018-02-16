71304513fc00162219b4145a6dbbe29b23635847
codeAWgit <36483111+codeAWgit@users.noreply.github.com>
2018-02-15 19:33:45 -0600

    These are the correct map URLs
    I changed the script information to work on all my computers and changed back the map URL to open the zoomed out map on Google.

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ $(document).ready(() => {
         $.getJSON('http://api.open-notify.org/iss-now.json', function (json) {
             let lat = JSON.stringify(json.iss_position.latitude).replace(/\"/g, '');
             let long = JSON.stringify(json.iss_position.longitude).replace(/\"/g, '');
-            let mapsUrl = 'https://www.google.com/maps/search/' + lat + ', ' + long;
+             let mapsUrl = 'https://www.google.com/maps/search/' + lat + ',' + long + '/@' + lat + ',' + long + ',1z'; 
             
             $(".message").html('Latitude: ' + lat + '<br/><br/> Longitude: ' + long + '<br/><br/><a href="' + mapsUrl + '" target="_blank">Click to see on Google Maps</a>' + '<br/><br/>' + '<span id="json">' + JSON.stringify(json) + '</span>');
             
